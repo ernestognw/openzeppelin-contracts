0f7156a8618c1c425958e1e86d0d8f63323be075
ernestognw <ernestognw@gmail.com>
2023-01-03 14:42:58 -0600

    Minor

--- contracts/token/ERC20/ERC20.sol
+++ contracts/token/ERC20/ERC20.sol
@@ -49,7 +49,7 @@ contract ERC20 is Context, IERC20, IERC20Metadata {
      * {decimals} you should overload it.
      *
      * All two of these values are immutable: they can only be set once during
-     * construction.
+     * construction
      */
     constructor(string memory name_, string memory symbol_) {
         _name = name_;
