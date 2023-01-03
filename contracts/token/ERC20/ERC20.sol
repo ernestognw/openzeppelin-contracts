f839a25338b8734fd8c51082e83d9524584ac5a3
ernestognw <ernestognw@gmail.com>
2023-01-03 12:49:29 -0600

    Test patch

--- contracts/token/ERC20/ERC20.sol
+++ contracts/token/ERC20/ERC20.sol
@@ -26,7 +26,7 @@ import "../../utils/Context.sol";
  * Additionally, an {Approval} event is emitted on calls to {transferFrom}.
  * This allows applications to reconstruct the allowance for all accounts just
  * by listening to said events. Other implementations of the EIP may not emit
- * these events, as it isn't required by the specification.
+ * these events, as it isn't required by the specification
  *
  * Finally, the non-standard {decreaseAllowance} and {increaseAllowance}
  * functions have been added to mitigate the well-known issues around setting
