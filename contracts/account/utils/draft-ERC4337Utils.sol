271818d476bf311f99dcddf723973314e0a890bd
Ernesto García <ernestognw@gmail.com>
2024-11-27 18:37:00 -0600

    Extend ERC-4337 docs

--- contracts/account/utils/draft-ERC4337Utils.sol
+++ contracts/account/utils/draft-ERC4337Utils.sol
@@ -9,6 +9,27 @@ import {Packing} from "../../utils/Packing.sol";
 /**
  * @dev Library with common ERC-4337 utility functions.
  *
+ * Example usage:
+ *
+ * ```solidity
+ * contract Example {
+ *     using ERC4337Utils for *;
+ *
+ *     function _foo(PackedUserOperation memory userOp) internal {
+ *         // Parse
+ *         (address aggregator, uint48 validAfter, uint48 validUntil) = validationData.parse();
+ *
+ *         // Get values
+ *         address factory = userOp.factory();
+ *         bytes calldata factoryData = userOp.factoryData();
+ *         uint256 verificationGasLimit = userOp.verificationGasLimit();
+ *         uint256 callGasLimit = userOp.callGasLimit();
+ *         // ...
+ *
+ *     }
+ * }
+ * ```
+ *
  * See https://eips.ethereum.org/EIPS/eip-4337[ERC-4337].
  */
 library ERC4337Utils {
